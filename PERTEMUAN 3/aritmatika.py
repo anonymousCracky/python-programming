91e4a5174b54d50588dc60d0bf16e28379bd4a32
anonymousCracky <fahriesaori@gmail.com>
2024-07-30 09:40:54 +0700

    Add files via upload

---
+++ PERTEMUAN 3/aritmatika.py
@@ -0,0 +1,20 @@
+bil1 = 10
+bil2 = 2
+
+print("penjumlahan :", bil1 + bil2)
+
+print("pengurangan :", bil1 - bil2)
+
+print("perkalian :", bil1 * bil2)
+
+print("pembagian :", bil1 / bil2)
+
+print("perpangkatan :", bil1 ** bil2)
+
+print("modulus :", bil1 % bil2)
+
+print("pembagian bulat :", bil1 // bil2)
+
+
+
+
