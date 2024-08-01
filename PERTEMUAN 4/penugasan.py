f3dc3c218c4a540bc52db40ff248d0e0835eabb8
anonymousCracky <fahriesaori@gmail.com>
2024-08-01 12:13:03 +0700

    Add files via upload

---
+++ PERTEMUAN 4/penugasan.py
@@ -0,0 +1,25 @@
+x = 100
+
+#Penjumlahan
+x +=3
+print("penjumLahan: ", x +3)
+
+print("pengurahan: ", x -3)
+#pengurangan
+x -=3
+
+print("perkalian: ", x *3)
+#Perkalian
+x *=3
+
+print("pembagian: ", x /3)
+#Pembagian
+x /=3
+
+print("modulus: ", x %3)
+#Modulus
+x %=3
+
+print("perpangkatan: ", x **3)
+#Perpangkatan
+x **=3
