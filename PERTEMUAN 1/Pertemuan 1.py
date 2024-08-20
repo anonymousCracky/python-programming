c3775f2cd739bad6c3ee8d1ed485169d21624e0a
anonymousCracky <fahriesaori@gmail.com>
2024-08-20 09:23:26 +0700

    Add files via upload

---
+++ PERTEMUAN 1/Pertemuan 1.py
@@ -0,0 +1,7 @@
+nama = "Mohammad Fahri Mi'raj Tsaori"
+kelas = "TKJ X A"
+hobi = "Jogging"
+
+print("perkemalkan nama saya",nama)
+print(f"saya kelas{kelas}")
+print(f"hobi saya{hobi},saya sangat senang melakukanya")
